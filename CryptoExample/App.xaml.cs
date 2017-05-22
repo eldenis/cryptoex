ecffe8a18adeb525b1afe01ee815d6146d820d6e
Denis J. González <dns.gonz@gmail.com>
2017-05-22 00:32:33 -0500

    Adding main classes

--- CryptoExample/App.xaml.cs
+++ CryptoExample/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MahApps.Metro;
 
 namespace CryptoExample
 {
@@ -13,5 +14,14 @@ namespace CryptoExample
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            base.OnStartup(args);
+            DispatcherUnhandledException += (s, e) =>
+            {
+                MessageBox.Show($"{e.Exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+        }
     }
 }
--- CryptoExample/MainWindow.xaml.cs
+++ CryptoExample/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +13,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ReactiveUI;
 
 namespace CryptoExample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+            ViewModel = DataContext as MainViewModel;
+
+            ViewModel?.ShowErrorMessage.Subscribe(ex =>
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            });
+
+        }
+
+        MainViewModel ViewModel { get; set; }
     }
 }
